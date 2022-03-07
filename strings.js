789618968c8c93278636b678357773054f67299a
aryan1403 <aaryan14032006@email.com>
2022-03-07 19:08:20 +0530

    strings done

---
+++ strings.js
@@ -0,0 +1,7 @@
+let str = "Meow";
+
+console.log(str.charAt(0)); // Pulling a speicific char from a string
+console.log(str.length); // length of the string
+console.log(str.replace("ow", "aryan")); // Replace a sequence of chars
+console.log(str.substring(0, 3)); // Pull out a sub string from a string
+console.log(str.concat("Aryan")); // Add this string after the original one
