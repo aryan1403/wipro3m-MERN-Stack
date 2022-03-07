4eb5653984f1fa52639ec1ebcf829c004e3508f8
aryan1403 <aaryan14032006@email.com>
2022-03-07 18:38:16 +0530

    variables done

---
+++ variables.js
@@ -0,0 +1,17 @@
+// Numbers - Mathematical numbers
+var x = 10;
+var y = 15.07;
+
+//Strings - Sequence of Characters
+var name = "Aaryan";
+var name2 = "Prasad";
+
+// Booleans - Value Either true or false
+var b = true;
+
+
+// const - A value that is constant i.e it cannot be changed
+const c = 45;
+
+console.log("Sum = ", (x + y));
+console.log(name, name2);
