6bb80ca39678f32aade54888c2e295a9cda2d11c
aryan1403 <aaryan14032006@email.com>
2022-03-07 19:07:58 +0530

    arrays done

---
+++ arrays.js
@@ -0,0 +1,26 @@
+let arr = ["Aaryan", "Prasad", "Aditya", "Harshavardhan", 120, true];
+        //  0          1         2         3               4    5
+
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+
+// Printing
+// console.log(arr.toString());
+
+/* for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]);
+}*/
+
+// arr.push("meow");
+// arr.push("meow2");
+
+// console.log(arr.toString());
+// arr.forEach(value => console.log(value)); // foreach method
+
+/*
+arr.map((value) => {
+    console.log(value);
+}); */
+
+
